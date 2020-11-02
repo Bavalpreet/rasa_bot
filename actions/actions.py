35b49290a8400e038a7675259be5be40eb2ae15e
bavalpreet singh <bavalpreet.singh25@gmail.com>
2020-11-02 11:51:53 +0530

    second commit

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +25,16 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class Action_repair_hull(Action):
+
+    def name(self) -> Text:
+        return "action_repair_hull"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="There are two scenarios where a boat hull can be damaged – one is when the hull is damaged above the waterline, the second when it is damaged below the waterline.For first scenario take it out and dry it thoroughly.For hull repair, a basic fibreglass repair kit is used, using which the damaged section is removed in a circular cut. The part can be then patched using either fibreglass and the proper adhesives or the putties available.")
+
+        return []
