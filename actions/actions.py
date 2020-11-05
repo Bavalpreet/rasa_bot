f4f695670c51e033254824002d84f6ea09f3c3a0
bavalpreet singh <bavalpreet.singh25@gmail.com>
2020-11-05 12:48:58 +0530

    fourth commit

--- actions/actions.py
+++ actions/actions.py
@@ -38,3 +38,30 @@ class Action_repair_hull(Action):
         dispatcher.utter_message(text="There are two scenarios where a boat hull can be damaged – one is when the hull is damaged above the waterline, the second when it is damaged below the waterline.For first scenario take it out and dry it thoroughly.For hull repair, a basic fibreglass repair kit is used, using which the damaged section is removed in a circular cut. The part can be then patched using either fibreglass and the proper adhesives or the putties available.")
 
         return []
+
+
+class Action_repair(Action):
+
+    def name(self) -> Text:
+        return "action_repair"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message['entities']
+        print(entities)
+
+        for e in entities:
+            if e['entity'] == 'boat_part':
+                name = e['value']
+
+            if name == "hull":
+                message = "There are two scenarios where a boat hull can be damaged – one is when the hull is damaged above the waterline, the second when it is damaged below the waterline.For first scenario take it out and dry it thoroughly.For hull repair, a basic fibreglass repair kit is used, using which the damaged section is removed in a circular cut. The part can be then patched using either fibreglass and the proper adhesives or the putties available."
+
+            if name == "core":
+                message = "Core damage primarily happens due to the intrusion of water through hull-fittings or deck-fittings"
+            
+        dispatcher.utter_message(text=message)
+
+        return []
